f1deea26da840fda63c419fd48e28a11f1a7bc42
kishan <kishanj918@gmail.com>
2019-07-15 12:26:07 +0530

    Map-navigaion Workgin

---
+++ app/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import HomeScreen from './Home/home';
+import Map from './Map/map';
+
+
+const TabNavigator = createBottomTabNavigator({
+  Home: HomeScreen,
+  Map: Map,
+},
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let IconComponent = Ionicons;
+        let iconName;
+        if (routeName === 'Home') {
+          iconName = `ios-home`;
+          // Sometimes we want to add badges to some icons. 
+          // You can check the implementation below.
+          // IconComponent = HomeIconWithBadge;
+        } else if (routeName === 'Map') {
+          iconName = `ios-map`;
+        }
+
+        // You can return any component that you like here!
+        return <IconComponent name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    },
+  }
+);
+
+export default createAppContainer(TabNavigator);
